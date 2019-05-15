97a7dd5d809a18d3ca85ab3c3762e55b015a24d4
kyungryun <kyung_ryun@naver.com>
2019-05-15 15:46:11 +0900

    ranking parsing

--- bgg_game_contents.py
+++ bgg_game_contents.py
@@ -13,27 +13,33 @@ cnt = 0
 game_contents = []
 
 def get_contents(id):
-    xml = requests.get("https://boardgamegeek.com/xmlapi2/thing?id=%s?comments=0&stats=1&videos=0"%id).text
+    xml = requests.get("https://boardgamegeek.com/xmlapi2/thing?id=%s?comments=1&stats=1"%id).text
     soup = BeautifulSoup(xml, 'lxml')
 
-    #game Thumbnail
-    thumbnail = soup.find('thumbnail').text
+    # game Thumbnail
+    thumbnail = ''
+    try:
+        thumbnail = soup.find('thumbnail').text
+    except:
+        thumbnail = 'https://cf.geekdo-images.com/micro/img/QZDNfKAPYlXkZg265NxdjgShBXY=/fit-in/64x64/pic1657689.jpg'
 
-    polls = soup.findAll('poll')
-    #suggested_numPlayers
+
+    # suggested_numPlayers
     # votes id 3 : Best 2 : Recommended 1 : Not Recommended
     suggested_numPlayers = {}
 
-    #language dependence levels
-    #level 1 : No necessary in-game text
-    #level 2 : Some necessary text - easily memorized or small crib sheet
-    #level 3 : Moderate in-game text - needs crib sheet or paste ups
-    #level 4 : Extensive use of text - massive conversion needed to be playable
-    #level 5 : Unplayable in another language
-    language_Dependence = {}
+    # language dependence levels
+    # level 0 : No votes
+    # level 1 : No necessary in-game text
+    # level 2 : Some necessary text - easily memorized or small crib sheet
+    # level 3 : Moderate in-game text - needs crib sheet or paste ups
+    # level 4 : Extensive use of text - massive conversion needed to be playable
+    # level 5 : Unplayable in another language
+    language_Dependence = {0:0}
+    polls = soup.findAll('poll')
 
     for poll in polls:
-        if poll['name'] == 'suggested_numplayers':
+        if poll['name'] == 'suggested_numplayers' and int(poll['totalvotes']) != 0:
             numplayers = poll.findAll('results')
             for numplayer in numplayers:
                 votes = numplayer.findAll('result')
@@ -41,17 +47,17 @@ def get_contents(id):
                     1 : int(votes[2]['numvotes']) ,\
                     2 : int(votes[1]['numvotes']) ,\
                     3 : int(votes[0]['numvotes'])}
-        if poll['name'] == 'language_dependence':
+        if poll['name'] == 'language_dependence' and int(poll['totalvotes']) != 0:
             dependences = poll.findAll('result')
             for dependence in dependences:
                 language_Dependence[int(dependence['level'])] = int(dependence['numvotes'])
 
-    #Best , Recommended, NotRecommended Players
+    # Best , Recommended, NotRecommended Players
     playersRecommended = []
     playersBest = []
     playersNotRecommended = []
 
-    for num in suggested_numPlayers:
+    for num in suggested_numPlayers :
         values = list(suggested_numPlayers[num].values())
         keys = list(suggested_numPlayers[num].keys())
         maxVotes = keys[values.index(max(values))]
@@ -66,44 +72,61 @@ def get_contents(id):
     keys = list(language_Dependence.keys())
     values = list(language_Dependence.values())
 
-    #suggested Language Dependence
+    # suggested Language Dependence
     suggested_language_Dependence = keys[values.index(max(values))]
 
-    #Num Players
+    # Num Players
     minPlayer = soup.find('minplayers')['value']
     maxPlayer = soup.find('maxplayers')['value']
 
-    #Playing Time
+    # Playing Time
     minPlayTime = soup.find('minplaytime')['value']
     maxPlayTime = soup.find('maxplaytime')['value']
 
+    # Min Age
     minAge = soup.find('minage')['value']
 
-    #game category, mechanic
+    # game category, mechanic, family, expansion, designer, artist, publisher
     boardgameCategory = []
     boardgameMechanic = []
-    tag = soup.findAll('tag')
-    for eachtag in tag:
+    boardgameDesigner = []
+    boardgameFamily = []
+    boardgameExpansion = []
+    boardgameArtist = []
+    boardgamePublisher = []
+
+    for eachtag in soup.findAll('link'):
         if eachtag['type'] == 'boardgamecategory':
             boardgameCategory.append(eachtag['id'])
         elif eachtag['type'] == 'boardgamemechanic':
             boardgameMechanic.append(eachtag['id'])
+        elif eachtag['type'] == 'boardgamedesigner':
+            boardgameDesigner.append(eachtag['id'])
+        elif eachtag['type'] == 'boardgamefamily':
+            boardgameFamily.append(eachtag['id'])
+        elif eachtag['type'] == 'boardgameexpansion':
+            boardgameExpansion.append(eachtag['id'])
+        elif eachtag['type'] == 'boardgameArtist':
+            boardgameArtist.append(eachtag['id'])
+        elif eachtag['type'] == 'boardgamePublisher':
+            boardgamePublisher.append(eachtag['id'])
 
-    #group ranks
-    ranks = soup.find('ranks')
+    # group ranks
+    # { subgroup name : { rank : bayesaverage } }
+    ranks = {}
+    for ranking in soup.findAll('rank'):
+        ranks[ranking['name']] = {
+                    ranking['value'] if ranking['value'].isnumeric() else 'N/A' :
+                        ranking['bayesaverage'] if not ranking['bayesaverage'] == 'Not Ranked' else 'N/A'}
     print(ranks)
-    
-    #ratings
+    # ratings
     usersrated = soup.find('usersrated')['value']
     average = soup.find('average')['value']
     bayesaverage = soup.find('bayesaverage')['value']
 
-    #weights
+    # weights
     numweights = soup.find('numweights')['value']
     averageweight = soup.find('averageweight')['value']
 
 collection = db.gameContents
-get_contents(174430)
-
-#for n in game_contents:
-    #collection.insert(n)
+get_contents(279990)
