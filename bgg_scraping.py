89fe3deb9c73e1dcb1618e42b8b9ffe09e5760fd
kyungryun <kyung_ryun@naver.com>
2019-05-20 17:39:13 +0900

    get data & insert to db

--- bgg_scraping.py
+++ bgg_scraping.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 import requests
 import re
 import pymongo
+import time
+
+page = 1
+conn = pymongo.MongoClient('127.0.0.1',27017)
+db = conn.bggDB
+game_page = db.game_page
+game_category = db.game_category
+game_mechanic = db.game_mechanic
+
 
 # Find list page
 def find_last_page():
@@ -11,92 +21,74 @@ def find_last_page():
     last_page = int(re.findall('\d+',soup.select('#main_content div.infobox .fr a')[-1].text)[0])
     return last_page
 
-# insert to DB without duplicate
-def insert_game(list):
-    global collection
+# crawling game page 1 to last_page
+def get_game_page(page):
+    url = 'https://boardgamegeek.com/browse/boardgame/page/'+ str(page)
+    req = requests.get(url).text
+    soup = BeautifulSoup(req, 'html.parser')
+    list = []
+    items = soup.select('#row_')
+    for item in items:
+        try:
+            rank = int(item.select('td.collection_rank')[0].text.strip())
+        except:
+            rank = 0
+        title = item.select('td.collection_objectname a')[0]
+        try:
+            year = int(re.findall('\d+', item.find('span',{'class' : 'smallerfont dull'}).text)[0])
+        except:
+            year = 0
+        id = title.get('href').split('/')[2]
+        list.append({ 'id' : int(id) , \
+                            'title' : title.text.strip() , \
+                            'year' : year,\
+                            'rank' : rank})
     for n in list:
         try:
-            collection.insert(n)
+            game_page.insert(n)
         except:
-            collection.update(
+            game_page.update(
                 {'id' : n['id']},
                 {'$set' : {'rank' : n['rank']}}
             )
 
-    print("insert %d games complete" % len(game_list))
+    print("total %d games " % game_page.count() )
 
-# parsing list using page_source
-# flag 0 : gamepage 1 : gamecategory 2 : gamemechanic
-def parsing_list(page_source, flag):
-    soup = BeautifulSoup(page_source, 'html.parser')
-    list = []
-    if flag == 0:
-        items = soup.select('#row_')
-        for item in items:
-            rank = item.select('td.collection_rank')[0].text.strip()
-            title = item.select('td.collection_objectname a')[0]
-            year = re.findall('\d+', n.select('td.collection_objectname span')[0].text.strip())[0]
-            id = title.get('href').split('/')[2]
-            list.append({ 'id' : int(id) , \
-                                'title' : title.text.strip() , \
-                                'year' : int(year),\
-                                'rank' : int(rank)})
-
-    elif flag == 1:
-        items = soup.find('table', {'class' : 'forum_table'}).findAll('a')
-        for item in items:
-            id = item['href'].split('/')[2]
-            name = item.text.strip()
-            list.append({ 'id' : id, 'category' : name} )
-    elif flag == 2:
-        items = soup.find('table', {'class' : 'forum_table'}).findAll('a')
-        for item in items:
-            id = item['href'].split('/')[2]
-            name = item.text.strip()
-            list.append({ 'id' : id, 'mechanic' : name} )
-
-    insert_game(list)
-    print("insert DB...")
-
-
-
-# crawling game page 1 to last_page
-def crawling_game_page(page, last_page):
-    page_source = ""
-    while 1:
-        if page != 2:
-            url = 'https://boardgamegeek.com/browse/boardgame/page/'+ str(page)
-            req = requests.get(url)
-            page_source = page_source + req.text
-            if page%10 == 0:
-                parsing_list(page_source, 0)
-                page_source = ""
-            page = page + 1
-        else:
-            parsing_list(page_source, 0)
-            break
-
-def crawling_contents(domain):
+def get_contents(domain):
     page_source = ""
     url = 'https://boardgamegeek.com/browse/%s'%domain
-    req = requests.get(url)
-    page_source = page_source + req.text
-    soup = BeautifulSoup(page_source, 'html.parser')
-    
+    req = requests.get(url).text
+    soup = BeautifulSoup(req, 'html.parser')
 
+    items = soup.find('table', {'class' : 'forum_table'}).findAll('a')
+    list = []
+    for item in items:
+        id = item['href'].split('/')[2]
+        name = item.text.strip()
+        if domain == 'boardgamecategory':
+            list.append({ 'id' : id, 'category' : name} )
+        else:
+            list.append({ 'id' : id, 'mechanic' : name} )
+    for n in list:
+        if domain == 'boardgamecategory':
+            game_category.insert(n)
+        else:
+            game_mechanic.insert(n)
 
-"""
-page = 1
-conn = pymongo.MongoClient('127.0.0.1',27017)
-db = conn.bggDB
-gameList = db.gameList
-gameCategories = db.categories
-gamemechanic = db.mechanic
+# if db.count() == 0 insert to db
+if game_page.count() == 0:
 
-last_page = find_last_page()
-print("totalgame list pages %d" % last_page)
+    last_page = find_last_page()
+    print("totalgame list pages %d" % last_page)
 
-print("start crawling")
-crawling_game_page(page, last_page)
-collection.create_index('id', unique=True)
-"""
+    print("game page crawling")
+    pool = Pool(processes=4)
+    pool.map(crawling_game_page,range(1, last_page+1))
+
+if game_category.count() == 0:
+    print("game category crawling")
+    get_contents('boardgamecategory')
+
+if game_mechanic.count() == 0:
+    print("game mechanic crawling")
+    get_contents('boardgamemechanic')
